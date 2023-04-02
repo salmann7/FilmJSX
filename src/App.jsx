6586d47ea9b730aea49d7fe94df52563ae91d4ae
salman <salmann9977@gmail.com>
2023-04-02 17:48:04 +0530

    nav bar code mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
 import { Routes, Route, Link } from "react-router-dom";
+
 import Movies from './pages/Movies/Movies';
 import MovieInformation from './pages/MovieInformation/MovieInformation';
 import Actor from './pages/Actor/Actor';
 import Profile from './pages/Profile/Profile';
+import Navbar from './components/NavBar/Navbar';
+
+import useStyles from './styles';
 
 const App = () => {
+
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>  
       <CssBaseline />
-      <main>
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar}/>
         <Routes>
           <Route exact path='/' element={<Movies />}/>
           <Route exact path='/movie/:id' element={<MovieInformation />}/>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import App from './App'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import App from './App';
+
+const theme = createTheme({});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
